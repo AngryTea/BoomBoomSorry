d6ad09bf7433cd5402c54de09e79316fa1628056
enrico-kaack <e.kaack@live.de>
2019-05-25 15:14:25 +0200

    Added: state machine for moves

---
+++ CurrentTurnState.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var currentPlayer = PlayerInfo.PLAYER1
+enum {SELECT_FIGURE, CHOOSE_MOVE_OR_WEAPON, TARGET, ROLL_DICE_TO_HIT, ROLL_DICE_TO_MOVE, CHOOSE_MOVE }
+
+var currentState = SELECT_FIGURE
